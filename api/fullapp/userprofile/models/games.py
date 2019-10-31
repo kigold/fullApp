7091e5f1c02da2a382068b2b08ade5c4e658bd63
kingsley <kingsleybox@yahoo.com>
2019-10-31 21:59:49 +0100

    Added games model

---
+++ api/fullapp/userprofile/models/games.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Game(models.Model):    
+    away_user_id = models.IntegerField() 
+    away_score = models.IntegerField()   
+    away_team_id = models.IntegerField()    
+    home_user_id = models.IntegerField()
+    home_score = models.IntegerField()
+    home_team_id = models.IntegerField()
+    penalty_shootout = models.BooleanField()  
